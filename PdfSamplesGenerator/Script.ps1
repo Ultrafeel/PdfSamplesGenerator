15790ed192537610844cd097c940dcd0ce9d997e
Ultrafeel <agde@tut.by>
2016-08-25 15:23:06 +0300

    Prettify

--- PdfSamplesGenerator/Script.ps1
+++ PdfSamplesGenerator/Script.ps1
@@ -5,48 +5,48 @@
 
 function printto
 {
-	param( [string]$file, [string]$printer)
+  param([string]$file,[string]$printer)
 
-	if ($printer -ne $null)
-	{
-		Start-Process  –FilePath $file -ArgumentList $printer -Verb "printto"  -Wait
-	}
-	else
-	{
-		Start-Process  –FilePath $file  -Verb "print"   -Wait
+  if ($printer -ne $null)
+  {
+    Start-Process –FilePath $file -ArgumentList $printer -Verb "printto" -Wait
+  }
+  else
+  {
+    Start-Process –FilePath $file -Verb "print" -Wait
 
-	}
+  }
 
 }
-$printto =  #= "d:\INSTALL\!office\Bullzip\files\printto.exe"
-$pdftk  =  Get-Command "pdftk" -ErrorAction SilentlyContinue
+$printto = #= "d:\INSTALL\!office\Bullzip\files\printto.exe"
+$pdftk = Get-Command "pdftk" -ErrorAction SilentlyContinue
 if ($pdftk -eq $null)
-{ $pdftk  = "C:\Program Files (x86)\PDFtk\bin\pdftk.exe" }
+{ $pdftk = "C:\Program Files (x86)\PDFtk\bin\pdftk.exe" }
 
-$u7z  =  Get-Command "7z" -ErrorAction SilentlyContinue
+$u7z = Get-Command "7z" -ErrorAction SilentlyContinue
 if ($u7z -eq $null)
-{ $u7z  = "C:\Program Files (x86)\Universal Extractor\bin\7z.exe" }
+{ $u7z = "C:\Program Files (x86)\Universal Extractor\bin\7z.exe" }
 
 function WaitForFile ($file)
 {
-	[int]$i=10000
-	for (; $i -gt 0 -and !(Test-Path $file); $i--) 
-		{ Start-Sleep 10 }
-	return ($i -gt 0);
+  [int]$i = 10000
+  for (; $i -gt 0 -and !(Test-Path $file); $i --)
+  { Start-Sleep 10 }
+  return ($i -gt 0);
 }
 
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-function msgBoxRetryCancel($x)
+function msgBoxRetryCancel ($x)
 {
-		# “Продолжить” или “Отменить”
+  # “Продолжить” или “Отменить”
 
-	$OUTPUT=  [System.Windows.Forms.MessageBox]::Show($x, 
-		'Генератор PDF образцов:PowerShell', 
-	[Windows.Forms.MessageBoxButtons]::RetryCancel , 
-	[Windows.Forms.MessageBoxIcon]::Exclamation, #Information
-		 [Windows.Forms.MessageBoxDefaultButton]::Button1)
-	# [Windows.Forms.MessageBoxOptions]::ServiceNotification
-	return $OUTPUT;
+  $OUTPUT = [System.Windows.Forms.MessageBox]::Show($x,
+    'Генератор PDF образцов:PowerShell',
+    [Windows.Forms.MessageBoxButtons]::RetryCancel,
+    [Windows.Forms.MessageBoxIcon]::Exclamation,#Information
+    [Windows.Forms.MessageBoxDefaultButton]::Button1)
+  # [Windows.Forms.MessageBoxOptions]::ServiceNotification
+  return $OUTPUT;
 }
 function Print1 ($file)
 {
@@ -70,47 +70,47 @@ function Print1 ($file)
   ECHO $settings
   $settFile = $null
   $settingsBackFile = $null
-   if (Test-Path "$settings")
+  if (Test-Path "$settings")
   {
     $settFile = (Get-Item $settings)
-	 $settingsBackFileName = $SF1 + ".back"
-	 $settingsBackFile = Join-Path $settFile.Directory $settingsBackFileName|Get-Item  
-	  Remove-Item $settingsBackFile.FullName -Force;
-	  Move-Item $settFile.FullName $settingsBackFile.FullName -Force
-	  # Get-Item $settingsBackFile
+    $settingsBackFileName = $SF1 + ".back"
+    $settingsBackFile = Join-Path $settFile.Directory $settingsBackFileName | Get-Item
+    Remove-Item $settingsBackFile.FullName -Force;
+    Move-Item $settFile.FullName $settingsBackFile.FullName -Force
+    # Get-Item $settingsBackFile
     # rename-item $settFile $settingsBackFileName -Force
 
     #  $newSett = New-Item $(Join-Path $settFile.Directory ($settFile.name + ".new")) # $newSett = New-Item $(Join-Path $settFile.Directory ($settFile.name + ".new"))
-   # $newSett.Replace(($settFile.FullName) ,(Join-Path  $settFile.Directory   $settingsBackFileName ) ,($true) )
-   
+    # $newSett.Replace(($settFile.FullName) ,(Join-Path  $settFile.Directory   $settingsBackFileName ) ,($true) )
+
   }
-	else
-	{
-	 #$settingsBackFileName = $SF1 + ".back"
- 
-	}
+  else
+  {
+    #$settingsBackFileName = $SF1 + ".back"
+
+  }
   #(rename "$settings" "$SF1.back")
   $samplesTargetDirName = "Образцы"
   $sampleSuffix = "_образец"
-	$watermarkText = "OBRAZEC" # "образец"
-	$samplesTarget = Join-Path -Path $($file.Directory) -ChildPath $samplesTargetDirName
+  $watermarkText = "OBRAZEC" # "образец"
+  $samplesTarget = Join-Path -Path $($file.Directory) -ChildPath $samplesTargetDirName
   $sampleFileName = $file.basename + $sampleSuffix
 
 
-	while (!(Test-Path $samplesTarget))
-	{
-		$msg = "Нет папки `“$samplesTargetDirName`” по пути `“$($file.Directory)`”"
-		$OUTPUT = msgBoxRetryCancel($msg)
-		if ($OUTPUT -eq [System.Windows.Forms.DialogResult]::Cancel ) 
-		{ 
-			throw $msg;
-		} 
-	}
+  while (!(Test-Path $samplesTarget))
+  {
+    $msg = "Нет папки `“$samplesTargetDirName`” по пути `“$($file.Directory)`”"
+    $OUTPUT = msgBoxRetryCancel ($msg)
+    if ($OUTPUT -eq [System.Windows.Forms.DialogResult]::Cancel)
+    {
+      throw $msg;
+    }
+  }
 
-	$outFileS =	"$samplesTarget\$sampleFileName" 
-	$outFile =	("$outFileS"+ ".pdf")
-	
-	 # %CD%\out\demo.pdf
+  $outFileS = "$samplesTarget\$sampleFileName"
+  $outFile = ("$outFileS" + ".pdf")
+
+  # %CD%\out\demo.pdf
   ECHO "Save settings to \" $settings\""
   #ECHO "[PDF Printer]" > "$settings"
   #ECHO "output=$outFile" 			 >> "$settings"
@@ -118,9 +118,9 @@ function Print1 ($file)
   #ECHO showsettings=never 			>> "$settings"
   #ECHO showpdf=no >> "$settings"
 
- # Out-File "$settings" -Append -Encoding "unicode" -InputObject 	"$watermarkText"
- 
-	# Нельзя добавлять в конце пробелы!!
+  # Out-File "$settings" -Append -Encoding "unicode" -InputObject 	"$watermarkText"
+
+  # Нельзя добавлять в конце пробелы!!
   Out-File "$settings" -Encoding "unicode" -InputObject @"
 [PDF Printer]
   output=$outFile
@@ -137,51 +137,51 @@ function Print1 ($file)
   watermarkverticalposition=center
   watermarkhorizontalposition=center
   confirmoverwrite=no
-"@ 
-	#	>> "$settings"
-	#PrintToPrinter=Foxit Reader PDF Printer
-	#PrinterFirstPage=1
-	#PrinterLastPage=2
+"@
+  #	>> "$settings"
+  #PrintToPrinter=Foxit Reader PDF Printer
+  #PrinterFirstPage=1
+  #PrinterLastPage=2
 
   # ECHO "watermarktext=$watermarkText" >> "$settings"
- # ECHO >> "$settings"
+  # ECHO >> "$settings"
   "$file.FullName"
 
   printto ("""" + $file.FullName + """") ("""" + $PRINTERNAME + """") -ErrorAction Continue -ErrorVariable ProcessError
   #  Silently
-	If ($ProcessError) {
+  if ($ProcessError) {
 
-	  echo  $file.FullName + "не имеет печатающей программы" 
+    ECHO $file.FullName + "не имеет печатающей программы"
 
-		return;
-	}
+    return;
+  }
 
 
   # $printto.exe "in\example.rtf" "$PRINTERNAME"
-  $ptERRORLEVEL=$lastexitcode
-	# if ($ptERRORLEVEL -eq 0) $res11 = & $pdftk "$outFile" dump_data | Select-string -Pattern "PageMediaNumber: ([0-9]*)"
-	  if ( WaitForFile($outFile) )
-	{
-		$res11 = & $pdftk "$outFile" dump_data | Select-string -Pattern "PageMediaNumber: ([0-9]+)"
+  $ptERRORLEVEL = $lastexitcode
+  # if ($ptERRORLEVEL -eq 0) $res11 = & $pdftk "$outFile" dump_data | Select-string -Pattern "PageMediaNumber: ([0-9]*)"
+  if (WaitForFile ($outFile))
+  {
+    $res11 = & $pdftk "$outFile" dump_data | Select-String -Pattern "PageMediaNumber: ([0-9]+)"
 
-		$numOfPages = ($res11.Matches[0].Groups[1].value)
-		if ($numOfPages -gt 8)
-		{
-		$outFileCut = ("$samplesTarget\$sampleFileName"+"C.pdf") #($outFile
-		& $pdftk "$outFile" cat 1-8 output $outFileCut  verbose
-		if (Test-path  $outFileCut)
-		{
-			Remove-Item $outFile -Force;
-			Move-Item $outFileCut $outFile -Force
-		}
-		}
-	}
-	
+    $numOfPages = ($res11.Matches[0].Groups[1].value)
+    if ($numOfPages -gt 8)
+    {
+      $outFileCut = ("$samplesTarget\$sampleFileName" + "C.pdf") #($outFile
+      & $pdftk "$outFile" cat 1-8 output $outFileCut verbose
+      if (Test-Path $outFileCut)
+      {
+        Remove-Item $outFile -Force;
+        Move-Item $outFileCut $outFile -Force
+      }
+    }
+  }
+
   if ($settingsBackFile -ne $null -and $settingsBackFile.Exists) #(Test-Path "$settings.back")
-  { 
-	 Remove-Item -Force $settings
-	 move-Item -Force $settingsBackFile.FullName $SF1 
-   }
+  {
+    Remove-Item -Force $settings
+    Move-Item -Force $settingsBackFile.FullName $SF1
+  }
   elseif (Test-Path $settingsBackFileName)
   {
     Rename-Item -Force $settingsBackFileName $settfile.name
@@ -215,50 +215,50 @@ $archs =
 	
 
 
-$ArchsExts = ($archs | ForEach-Object { $_[0]})
+$ArchsExts = ($archs | ForEach-Object { $_[0] })
 
- $cont1 | Where Extension -In '.zip','.rar'
+$cont1 | Where Extension -In '.zip','.rar'
 #$nonArch = $cont1| Where-Object {$_.Extension -notin '.zip','.rar' }#,'.config'
 
 
- # foreach ($value in $cont1) 
- For($i = 1; $i -le $cont1.count; $i++)
-{ 
- 
-$value = $cont1[$i]
-Write-Progress -Activity “Generating samples” -status “ file $value” `
--percentComplete ($i / $cont1.count*100)
-# $cont1 | Select name
+# foreach ($value in $cont1) 
+for ($i = 1; $i -le $cont1.Count; $i++)
+{
 
-if ($value.Attributes -band  [System.IO.FileAttributes]::Directory)
-	  {
-		continue;  
-	  }
-   Write-Host $value.FullName
-    Write-Host $value
-	$fExt = $value.Extension
-	if ($fExt -ne $null)
-	{ $fExt.TrimStart('.') }
-    if ($fExt -in $ArchsExts)#docx
+  $value = $cont1[$i]
+  Write-Progress -Activity “Generating samples” -Status “ file $value” `
+     -PercentComplete ($i / $cont1.Count * 100)
+  # $cont1 | Select name
+
+  if ($value.Attributes -band [System.IO.FileAttributes]::Directory)
+  {
+    continue;
+  }
+  Write-Host $value.FullName
+  Write-Host $value
+  $fExt = $value.Extension
+  if ($fExt -ne $null)
+  { $fExt.TrimStart('.') }
+  if ($fExt -in $ArchsExts) #docx
+  {
+
+  }
+  else
+  {
+    if ($value.Extension -in ".rtf",".cdr",".jpg",".tif",".tiff",".doc",".docx",".indd")
     {
 
-	}
-	else  
-	{
-		if ($value.Extension -in ".rtf", ".cdr", ".jpg", ".tif", ".tiff", ".doc", ".docx", ".indd" )
-		{
-
-			Print1 ($value)
-		}
-		elseif ($value.Extension -eq ".pdf")
-		{
-			Print1 ($value)
-		}
-		
-      $value.FullPath
-      #break;
+      Print1 ($value)
     }
-   }
+    elseif ($value.Extension -eq ".pdf")
+    {
+      Print1 ($value)
+    }
+
+    $value.FullPath
+    #break;
+  }
+}
 
 
 
