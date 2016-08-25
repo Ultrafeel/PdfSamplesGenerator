dc3e49db7ca9de492188ebbb8e04ed5edcfc8831
Ultrafeel <agde@tut.by>
2016-08-25 21:20:36 +0300

    iter & broken

--- PdfSamplesGenerator/Script.ps1
+++ PdfSamplesGenerator/Script.ps1
@@ -4,10 +4,10 @@
 
 function EchoA
 {
-    for($i=0;$i -lt $args.length;$i++)
-    {
-        "Arg $i is <$($args[$i])>"
-    }
+  for ($i = 0; $i -lt $args.length; $i++)
+  {
+    "Arg $i is <$($args[$i])>"
+  }
 }
 function printto
 {
@@ -15,18 +15,18 @@ function printto
   #$err1;
   if ($printer -ne $null)
   {
-    Start-Process –FilePath $file -ArgumentList $printer -Verb "printto" -Wait -errorVariable err1
+    Start-Process –FilePath $file -ArgumentList $printer -Verb "printto" -Wait -ErrorVariable err1
   }
   else
   {
-    Start-Process –FilePath $file -Verb "print" -Wait -errorVariable err1
+    Start-Process –FilePath $file -Verb "print" -Wait -ErrorVariable err1
 
   }
-	if ($err1 -ne $null)
-	{
-		return $false
-		}
-	return $true;
+  if ($err1 -ne $null)
+  {
+    return $false
+  }
+  return $true;
 
 }
 $printto = #= "d:\INSTALL\!office\Bullzip\files\printto.exe"
@@ -59,9 +59,14 @@ function msgBoxRetryCancel ($x)
   # [Windows.Forms.MessageBoxOptions]::ServiceNotification
   return $OUTPUT;
 }
-function Print1 ($file)
-{
 
+#directory to process
+function Print1 ($file, [string]$obrazcyParentDir)
+{
+	if ($obrazcyParentDir -eq $null)
+	{
+		$obrazcyParentDir = $file.Directory
+		}
   # Set environment variables used by the batch file
 
   $PRINTERNAMe = "Bullzip PDF Printer"
@@ -73,7 +78,7 @@ function Print1 ($file)
   $LAPP = $env:APPDATA
   $SF1 = "settings.ini"
 
-  if ($LAPP.LENGTH -eq 0)
+  if ($LAPP.length -eq 0)
   {
     $LAPP = "$env:USERPROFILE\Local Settings\Application Data"
   }
@@ -104,13 +109,14 @@ function Print1 ($file)
   $samplesTargetDirName = "Образцы"
   $sampleSuffix = "_образец"
   $watermarkText = "OBRAZEC" # "образец"
-  $samplesTarget = Join-Path -Path $($file.Directory) -ChildPath $samplesTargetDirName
+
+  $samplesTarget = Join-Path -Path $obrazcyParentDir -ChildPath $samplesTargetDirName
   $sampleFileName = $file.basename + $sampleSuffix
 
 
   while (!(Test-Path $samplesTarget))
   {
-    $msg = "Нет папки `“$samplesTargetDirName`” по пути `“$($file.Directory)`”"
+    $msg = "Нет папки `“$samplesTargetDirName`” по пути `“$obrazcyParentDir`”"
     $OUTPUT = msgBoxRetryCancel ($msg)
     if ($OUTPUT -eq [System.Windows.Forms.DialogResult]::Cancel)
     {
@@ -158,7 +164,7 @@ function Print1 ($file)
   # ECHO >> "$settings"
   "$file.FullName"
 
-  $ptSuccess = printto $file.FullName  $PRINTERNAME 
+  $ptSuccess = printto $file.FullName $PRINTERNAME
   #  Silently-ErrorVariable ProcessError -ErrorAction Continue 
   if (!$ptSuccess) {
 
@@ -169,7 +175,7 @@ function Print1 ($file)
 
 
   # $printto.exe "in\example.rtf" "$PRINTERNAME"
-#  $ptERRORLEVEL = $lastexitcode
+  #  $ptERRORLEVEL = $lastexitcode
   # if ($ptERRORLEVEL -eq 0) $res11 = & $pdftk "$outFile" dump_data | Select-string -Pattern "PageMediaNumber: ([0-9]*)"
   if (WaitForFile ($outFile))
   {
@@ -199,36 +205,59 @@ function Print1 ($file)
   }
 }
 
-if ($args.Count -gt 0 -and $args[0].LENGTH -ge 0)
-{ $targetP = $args[0]
+
+function AlgA_Iter
+{
+	params($value, $obrazcyParentDir)
+	    if ($value.Extension -in ".rtf",".cdr",".jpg",".tif",".tiff",".doc",".docx",".indd")
+    {
+
+      Print1 ($value, $obrazcyParentDir)
+    }
+    elseif ($value.Extension -eq ".pdf")
+    {
+      Print1 ($value, $obrazcyParentDir)
+    }
+
+    $value.FullPath
+}
+
+if ($args.Count -gt 0 -and $args[0].length -ge 0)
+{ 
+	$targetP = $args[0]
 }
 else
 { $targetP = Get-Location }
+Algs($targetP , $false, $null)
+
+function Algs($targetP, [Boolean]$algAForB, $obrazcyParentDir)
+{
+	
+	[Boolean]$algAOnly = $algAForB
 
 $cont1 = Get-ChildItem $targetP
 
 
-$archs = 
+$archs =
 
-((	"7z"	)	,		"7z"	) 				,
-((	"xz"	)	,		"XZ"	)				,
-((	"zip"	)	,		"ZIP"	)			,
-((	"gz","gzip","tgz"	)	,	"GZIP"	)		,
-((	"bz2","bzip2","tbz2","tbz"),	"BZIP2"	)	,
-((	"tar"	)	,		"TAR"	)				,
-((	"wim","swm"	)	,		"WIM"	),
-((	"lzma"	)	,		"LZMA"	)	 ,
-((	"rar"	)	,		"RAR"	)	 ,
-((	"cab"	)	,		"CAB"	)	 ,
-((	"arj"	)	,		"ARJ"	)	 ,
-((	"z","taz")	,		"Z"	)		 ,
-((	"cpio"	)	,		"CPIO"	)
-	
+(("7z"),"7z"),
+(("xz"),"XZ"),
+(("zip"),"ZIP"),
+(("gz","gzip","tgz"),"GZIP"),
+(("bz2","bzip2","tbz2","tbz"),"BZIP2"),
+(("tar"),"TAR"),
+(("wim","swm"),"WIM"),
+(("lzma"),"LZMA"),
+(("rar"),"RAR"),
+(("cab"),"CAB"),
+(("arj"),"ARJ"),
+(("z","taz"),"Z"),
+(("cpio"),"CPIO")
+
 
 
 $ArchsExts = ($archs | ForEach-Object { $_[0] })
 
-$cont1 | Where Extension -In '.zip','.rar'
 #$nonArch = $cont1| Where-Object {$_.Extension -notin '.zip','.rar' }#,'.config'
 
 
@@ -249,99 +278,112 @@ for ($i = 1; $i -le $cont1.Count; $i++)
   Write-Host $value
   $fExt = $value.Extension
   if ($fExt -ne $null)
-  { $fExt  = $fExt.TrimStart('.') }
-  if ($fExt -in $ArchsExts) 
+  { $fExt = $fExt.TrimStart('.') }
+  if ($fExt -in $ArchsExts)
   {
 
-	 $archContT=  & $u7z "l" "-slt"  $value.fullname  
-
-	  $aafiles=@();
-
-	  $fileL = $null;
-	  $pathFoundMode= $false;
-	  $dirFoundMode= $false;
-
-	  for($i = 0; $i -lt $archContT.Count; ++$i)
+	  #algB
+	   if ($algAOnly)
 	  {
-		  [string]$acString = $archContT[$i]
-		  if ($acString.Length -eq 0)
-		  {
-			  if ($fileL -ne $null) 
-			  {
-				$aafiles.Insert( $fileL)
-				 $pathFoundMode = $false;
-				 $dirFoundMode= $false;
+		  continue;
+	}
+    $archContT = & $u7z "l" "-slt" $value.FullName
 
+    $aafiles = {@()}.Invoke(); #System.Collections.ObjectModel.Collection`1[System.Management.Automation.PSObject]
+
+    $fileL = $null;
+    $pathFoundMode = $false;
+    $dirFoundMode = $false;
+
+    for ($i = 0; $i -lt $archContT.Count;++ $i)
+    {
+      [string]$acString = $archContT[$i]
+      if ($acString.length -eq 0)
+      {
+        if ($fileL -ne $null -and $dirFoundMode)
+        {
+          $aafiles.Add($fileL)
+        }
+          $pathFoundMode = $false;
+          $dirFoundMode = $false;
+
+		$fileL = $null
+      }
+      else {
+
+        if (!$pathFoundMode)
+        {
+          $archParse = $acString | Select-String -Pattern "Path = (.*)"
+
+          if ($archParse -ne $null -and $archParse.Matches[0].Success)
+          {
+            $pathFoundMode = $true;
+
+            if ($fileL -eq $null)
+            {
+              $fileL = New-Object PSObject -Property @{ Path = $archParse.Matches[0].Groups[1].value; isdir = $false }
+            }
+			else
+			  {
+				   $fileL.Path = $archParse.Matches[0].Groups[1].value #never!
 			}
-		  }	
-		  else {
-		  
-			  if (!$pathFoundMode)
-			  {
-			   $archParse	= $acString|Select-String -Pattern "Path = (.*)" 
+          }
+			$archParse
+        }
+        elseif (!$dirFoundMode)
+        {
+          $archParse = $acString | Select-String -Pattern "Folder = (.*)"
 
-			  if ($archParse.Matches[0].Success)
-			  {
-				  $pathFoundMode = $true;
+          if ($archParse -ne $null -and $archParse.Matches[0].Success)
+          {
+            $dirFoundMode = $true;
+            if ($fileL -ne $null)
+            {
+              $fileL.isdir = ($archParse.Matches[0].Groups[1].value -eq "+")
 
-		  if ($fileL -ne $null) 
-			  {
-			 $fileL = New-Object PSObject -Property @{ Path=$archParse.Matches[0].Groups[1].value; isdir=$false }
-			}
-				  }
-				  }
-			  elseif (!$dirFoundMode)
-			  {
-	   $archParse	= $acString|Select-String -Pattern "Directory = (.*)" 
+            }
+            else
+            { $fileL } # never!
+          }
 
-			  if ($archParse.Matches[0].Success)
-			  {
-				  $dirFoundMode = $true;
- if ($fileL -ne $null) 
-			  {
-				  $fileL.isdir = ($archParse.Matches[0].Groups[1].value -eq "+")
+        }
 
-				  }
-				  else
-				  { $fileL } # never!
-				  }
+      }
+      $aafiles
+    }
 
-				  }
 
-		}
-		  $aafiles
+	# если папок нет
+	  if (($aafiles| Where-Object { $_.isdir }).Length -eq 0)
+	  {
+		 $TMPfullP = $value.FullName + "ext"
+		 & $u7z "e" $value.FullName "-o$TMPfullP" "-i!$arPath" "-y"
+
+	  
 	  }
-	 $archCont	= $archContT|Select-String -Pattern "Path = (.*)" 
+  <#  $archCont = $archContT | Select-String -Pattern "Path = (.*)"
 
-	 $archCont
-	  $archCont2 = $archCont | Where-Object { $_.Matches[0].Success} | Select-Object -Skip 1 | select @{Name="FName";  Expression= {$_.Matches[0].Groups[1].value}}
-	
-	  $archCont2
-	  $TMPfullP = $value.fullname + "ext"  
-	  foreach ($archF in $archCont2)
-	  {
-		  $arPath =  $archF.FName
-		  $apSpl = Split-Path $arPath
-		 $apSplit =  $arPath.Split('\')
-$apSplit
+    $archCont
+    $archCont2 = $archCont | Where-Object { $_.Matches[0].Success } | Select-Object -Skip 1 | select @{ name = "FName"; Expression = { $_.Matches[0].Groups[1].value } }
 
-		 & $u7z  "e" $value.fullname  "-o$TMPfullP" "-i!$arPath" "-y"
-	 }
+    $archCont2
+    $TMPfullP = $value.FullName + "ext"
+    foreach ($archF in $archCont2)
+    {
+      $arPath = $archF.FName
+      $apSpl = Split-Path $arPath
+      $apSplit = $arPath.Split('\')
+      $apSplit
+
+      & $u7z "e" $value.FullName "-o$TMPfullP" "-i!$arPath" "-y"
+    }
+	  #>
   }
   else
   {
-	  continue;
-    if ($value.Extension -in ".rtf",".cdr",".jpg",".tif",".tiff",".doc",".docx",".indd")
-    {
-
-      Print1 ($value)
-    }
-    elseif ($value.Extension -eq ".pdf")
-    {
-      Print1 ($value)
-    }
-
-    $value.FullPath
+	 
+    continue; #TODO
+    AlgA_Iter $value  $obrazcyParentDir 
     #break;
   }
 }
